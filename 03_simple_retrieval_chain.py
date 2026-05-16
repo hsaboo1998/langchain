094d23888a80b29cd3b2f5ffbed8016bc2633e58
hridaya saboo <hridaya.saboo@socgen.com>
2026-05-16 10:33:44 +0530

    uncommit changes

--- 03_simple_retrieval_chain.py
+++ 03_simple_retrieval_chain.py
@@ -28,36 +28,36 @@ chunks = text_splitter.split_documents(document)
 # print(min([len(chunk.page_content) for chunk in chunks]), max([len(chunk.page_content) for chunk in chunks]))
 # print(len(chunks))
 
-# # Embeddings and vector database
-# embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
-# chromadb = Chroma.from_documents(chunks, embeddings)
-# vectors = [embeddings.embed_documents([chunk.page_content]) for chunk in chunks]
-# # print(len(vectors))
+# Embeddings and vector database
+embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
+chromadb = Chroma.from_documents(chunks, embeddings)
+vectors = [embeddings.embed_documents([chunk.page_content]) for chunk in chunks]
+# print(len(vectors))
 
-# # similarity search
-# similar_chunks = chromadb.similarity_search("How to identify gender of a cat?", k=3)
-# # print(similar_chunks[0].page_content)
+# similarity search
+similar_chunks = chromadb.similarity_search("How to identify gender of a cat?", k=3)
+# print(similar_chunks[0].page_content)
 
-# # retrievers
-# # vectorstore retriever - simple wrapper around vector store
-# vector_store_retriever = chromadb.as_retriever(search_type='mmr', search_kwargs={"k": 3, "fetch_k": 10, "lambda_mult": 0.5})
-# # parent document retriever - first fetches small chunks and then looks up parent id for larger documents
-# vector_store = Chroma(collection_name='cats', embedding_function=embeddings)
-# store=InMemoryByteStore()
-# child_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-# parent_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
-# parent_document_retriever = ParentDocumentRetriever(vectorstore=vector_store, byte_store=store, 
-#                                                     child_splitter=child_splitter, parent_splitter=parent_splitter)
-# parent_document_retriever.add_documents(document)
-# # print(len(parent_document_retriever.invoke("How to identify gender of a cat?")[0].page_content))
+# retrievers
+# vectorstore retriever - simple wrapper around vector store
+vector_store_retriever = chromadb.as_retriever(search_type='mmr', search_kwargs={"k": 3, "fetch_k": 10, "lambda_mult": 0.5})
+# parent document retriever - first fetches small chunks and then looks up parent id for larger documents
+vector_store = Chroma(collection_name='cats', embedding_function=embeddings)
+store=InMemoryByteStore()
+child_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+parent_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+parent_document_retriever = ParentDocumentRetriever(vectorstore=vector_store, byte_store=store, 
+                                                    child_splitter=child_splitter, parent_splitter=parent_splitter)
+parent_document_retriever.add_documents(document)
+# print(len(parent_document_retriever.invoke("How to identify gender of a cat?")[0].page_content))
 
-# # retrieval chain
-# retrieval_chain = RetrievalQA.from_chain_type(
-#     llm=llm,
-#     chain_type='stuff', # all retrieved documents are concatenated together and passed to llm
-#     retriever=parent_document_retriever,
-#     return_source_documents=False
-# )
+# retrieval chain
+retrieval_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type='stuff', # all retrieved documents are concatenated together and passed to llm
+    retriever=parent_document_retriever,
+    return_source_documents=False
+)
 # print(retrieval_chain.invoke("Which cat has longest hair?"))
 
 # ----------------------------------- old style react agent ----------------------------------------------
